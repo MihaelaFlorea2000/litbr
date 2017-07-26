82029c469e2e7effd3df802e1acf788e453277ae
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2017-07-26 13:27:35 +0300

    Updated css index and js
    Added parallax effects

--- js/function.js
+++ js/function.js
@@ -5,6 +5,19 @@ $(document).ready(function(){
   jQuery(".title").fitText(1, { minFontSize: '5px', maxFontSize: '20px' });
 
 
+
+  $('.hero .fly').each(function(i){
+    setTimeout(function(){
+      $('.hero .fly').eq(i).addClass('is-showing');
+
+    }, 150 * (i*2+1));
+
+  });
+
+
+
+
+
   $(".hamburger").click(function(){
     window.setTimeout(function(){
       if($(".hamburger").not("collapsed")){
@@ -20,44 +33,6 @@ $(document).ready(function(){
   });
 
 
-  // // Select all links with hashes
-  // $('a[href*="#"]')
-  //   // Remove links that don't actually link to anything
-  //   .not('[href="#"]')
-  //   .not('[href="#0"]')
-  //   .click(function(event) {
-  //     // On-page links
-  //     if (
-  //       location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '')
-  //       &&
-  //       location.hostname == this.hostname
-  //     ) {
-  //       // Figure out element to scroll to
-  //       var target = $(this.hash);
-  //       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-  //       // Does a scroll target exist?
-  //       if (target.length) {
-  //         // Only prevent default if animation is actually gonna happen
-  //         event.preventDefault();
-  //         $('html, body').animate({
-  //           scrollTop: target.offset().top
-  //         }, 1000, function() {
-  //           // Callback after animation
-  //           // Must change focus!
-  //           var $target = $(target);
-  //           $target.focus();
-  //           if ($target.is(":focus")) { // Checking if the target was focused
-  //             return false;
-  //           } else {
-  //             $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
-  //             $target.focus(); // Set focus again
-  //           };
-  //         });
-  //       }
-  //     }
-  //   });
-
-
   	$('a[href^="#"]').on('click',function (e) {
   	    e.preventDefault();
 
@@ -70,4 +45,28 @@ $(document).ready(function(){
   	        window.location.hash = target;
   	    });
   	});
+
+
+
+
+
+});
+
+// Parallax
+// $(window).scroll(function(){
+//   var wScroll = $(this).scrollTop();
+//   console.log(wScroll);
+//   $('.hero h1').css({
+//     'transform' : 'translate(0px, '+ wScroll / 20 +'px)'
+//   });
+// });
+
+$(window).scroll(function(){
+  var wScroll = $(this).scrollTop();
+  var opacity = 1-(wScroll/500);
+  console.log(opacity);
+  $('.hero').css({
+    'transform' : 'translate(0px, '+ wScroll / 10 +'%)',
+    'opacity' : opacity
+  });
 });
