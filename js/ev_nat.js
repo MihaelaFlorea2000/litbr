ac47ccee1ae011c1888e9bfe9e7ea9897a594508
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2017-08-26 14:33:37 +0300

    adding parallax

--- js/ev_nat.js
+++ js/ev_nat.js
@@ -3,27 +3,26 @@
 
 $(document).ready(function(){
 
+  $('.cont .fly2').each(function(i){
+    setTimeout(function(){
+      $('.cont .fly2').eq(i).addClass('is-showing');
+    }, 150 * (i*3+1));
+  });
+  setTimeout(function(){
+    $('.cont .fly').each(function(i){
+      setTimeout(function(){
+        $('.cont .fly').eq(i).addClass('is-showing');
+      }, 150 * (i*3+1));
+    });
+  }, 1000);
 
-// // Meniu Open
-//   $(".hamburger").click(function(){
-//     window.setTimeout(function(){
-//       if($(".hamburger").not("collapsed")){
-//         $(".teste").css("margin-top", "200px");
-//       };
-//     }, 0.5);
-//     window.setTimeout(function(){
-//       if($(".hamburger").hasClass("collapsed")){
-//         $(".teste").css("margin-top", "20px");
-//       };
-//     }, 0.5);
-//
-//   });
+
+
 
 // Scrolling Dependant
   $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
+    var opacity = 1-(wScroll/500);
     console.log(wScroll);
-
-
-
   });
+});
