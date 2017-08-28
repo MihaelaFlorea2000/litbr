634120319895176f8f4e7cf10fa6ae79d8a0907b
MihaelaFlorea2000 <mihaelaflorea201@yahoo.com>
2017-08-28 18:26:46 +0300

    make the shells work

--- js/bac.js
+++ js/bac.js
@@ -15,33 +15,61 @@ $(document).ready(function(){
     });
   }, 400);
 
+
+  var shell_closed = $('.ship').width();
+  var shell_top;
+  var shell_left;
+  
 // Image Swap Shell
   $('.ship').mouseover(function(){
-    // $(this).delay(200).fadeOut(100, function(){
-    //   $(this).attr('src', 'img/shell_open.png').css({'width':'130px'}).fadeIn(100);
-    // });
-    var shell_closed = $(this).width();
     $(this).attr('src', 'img/shell_open.png');
+    if(window.matchMedia('(max-width: 890px) and (min-width: 820px)').matches) {
+      shell_left = 1.1;
+      shell_top = -7.8;
+    } else if (window.matchMedia('(max-width: 820px) and (min-width: 767px)').matches) {
+      shell_left = 1;
+      shell_top = -7.2;
+    } else if (window.matchMedia('(max-width: 767px) and (min-width: 670px)').matches){
+      shell_left = 1.2;
+      shell_top = -7.3;
+    } else if (window.matchMedia('(max-width: 670px) and (min-width: 600px)').matches) {
+      shell_left = 1.4;
+      shell_top = -7.3;
+    } else if (window.matchMedia('(max-width: 600px) and (min-width: 520px)').matches) {
+      shell_left = 1.6;
+      shell_top = -7.3;
+    } else if (window.matchMedia('(max-width: 520px) and (min-width: 440px)').matches) {
+      shell_left = 1.8;
+      shell_top = -7.3;
+    } else if (window.matchMedia('(max-width: 440px)').matches) {
+      shell_left = 2.1;
+      shell_top = -7.3;
+    } else {
+      shell_left = 1;
+      shell_top = -8.5;
+    }
+    console.log(shell_closed + 20)
     $(this).css({
-      'width': '130px',
-      'position':'relative',
-      'left':'1vw'
+      'width': shell_closed + 20 + 'px',
+      'position': 'relative',
+      'left': shell_left + 'vw'
     });
     $(this).parent().css({
       'position':'relative',
-      'top':'-4vw',
+      'top': shell_top + 'vh',
     });
   });
   $('.ship').mouseout(function(){
+    console.log(shell_closed)
     $(this).attr('src', 'img/shell_closed.png');
     $(this).css({
-      'width':'110px',
+      'width': shell_closed + 'px',
       'position':'relative',
-      'left':'0px'
+      'left':'0vw'
     });
     $(this).parent().css({
       'position':'relative',
-      'top':'0px',
+      'top':'0vh',
     });
   });
 
